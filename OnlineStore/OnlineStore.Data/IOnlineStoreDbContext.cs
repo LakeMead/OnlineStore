a7a17a638878869d293d242fc778962238b4c6b4
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-12 23:13:45 +0300

    Basics architecture of the system - Folders, Projects - Data, Web, Services, Tests; started DbContext #2; created Entity Models (only empty classes) #3 - closed #1

---
+++ OnlineStore/OnlineStore.Data/IOnlineStoreDbContext.cs
@@ -0,0 +1,46 @@
+﻿namespace OnlineStore.Data
+{
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    using OnlineStore.Models;
+
+    public interface IOnlineStoreDbContext
+    {
+        IDbSet<Order> Orders { get; set; }
+
+        IDbSet<Product> Products { get; set; }
+
+        IDbSet<Category> Categories { get; set; }
+
+        IDbSet<Label> Labels { get; set; }
+
+        IDbSet<Color> Colors { get; set; }
+
+        IDbSet<Review> Reviews { get; set; }
+
+        IDbSet<Comment> Comments { get; set; }
+
+        IDbSet<Rating> Ratings { get; set; }
+
+        IDbSet<Attribute> Attributes { get; set; }
+
+        IDbSet<Discount> Discounts { get; set; }
+
+        IDbSet<WishProduct> WishProducts { get; set; }
+
+        IDbSet<ContactFormFeedback> ContactFormFeedbacks { get; set; }
+
+        IDbSet<FrequentlyAskedQuestion> FrequentlyAskedQuestions { get; set; }
+
+        IDbSet<StaticPage> StaticPages { get; set; }
+
+        IDbSet<StaticPageSection> StaticPageSections { get; set; }
+
+        IDbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        int SaveChanges();
+    }
+}
---
+++ OnlineStore/OnlineStore.Data/OnlineStoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineStore.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using OnlineStore.Models;
+
+    public class OnlineStoreDbContext : IdentityDbContext<User>
+    {
+        public OnlineStoreDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static OnlineStoreDbContext Create()
+        {
+            return new OnlineStoreDbContext();
+        }
+    }
+}
