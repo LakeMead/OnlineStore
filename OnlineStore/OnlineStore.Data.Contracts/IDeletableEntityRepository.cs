9ab7076d87135441f07648fb584328ed8c83fcc7
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-13 16:15:20 +0300

    added GenericRepository.cs, DeletableEntityRepository.cs and the connected Interfaces #4

---
+++ OnlineStore/OnlineStore.Data.Contracts/IDeletableEntityRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineStore.Data.Contracts
+{
+    using System.Linq;
+
+    public interface IDeletableEntityRepository<T> : IGenericRepository<T> where T : class
+    {
+        IQueryable<T> AllWithDeleted();
+    }
+}
---
+++ OnlineStore/OnlineStore.Data/Repositories/DeletableEntityRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace OnlineStore.Data.Repositories
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    using OnlineStore.Data.Contracts;
+
+    public class DeletableEntityRepository<T> : GenericRepository<T>, IDeletableEntityRepository<T>
+         where T : class, IDeletableEntity, new()
+    {
+        public DeletableEntityRepository(DbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<T> All()
+        {
+            return base.All().OfType<T>().Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return base.All().OfType<T>();
+        }
+
+        public override void Delete(T entity)
+        {
+            entity.IsDeleted = true;
+            entity.DeletedOn = DateTime.Now;
+
+            this.Update(entity);
+        }
+
+        public override void DeleteById(object id)
+        {
+            var entity = this.GetById(id);
+
+            this.Delete(entity);
+        }
+    }
+}
