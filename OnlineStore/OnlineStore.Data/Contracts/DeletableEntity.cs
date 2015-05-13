502b1d1c9c4c7e68be629c60da71432f6e9bb4bb
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-13 12:32:31 +0300

    added DeletableEntity class and Interface - must be moved in separate project later #4

---
+++ OnlineStore/OnlineStore.Data/Contracts/DeletableEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineStore.Data.Contracts
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public abstract class DeletableEntity : IDeletableEntity
+    {
+        [Editable(false)]
+        public bool IsDeleted { get; set; }
+
+        [Editable(false)]
+        public DateTime? DeletedOn { get; set; }
+    }
+}
