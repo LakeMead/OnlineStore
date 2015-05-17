be9d6869bbd58404b0b79c21993e868b7ffac1c9
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-17 17:54:17 +0300

    added ILabelRepository.cs and LabelRepository.cs

---
+++ OnlineStore/OnlineStore.Data/Repositories/Contracts/ILabelRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineStore.Data.Repositories.Contracts
+{
+    using System.Linq;
+
+    using OnlineStore.Data.Contracts;
+    using OnlineStore.Data.Models;
+
+    public interface ILabelRepository : IDeletableEntityRepository<Label>
+    {
+        IQueryable<Label> GetByProductId(int id);
+    }
+}
---
+++ OnlineStore/OnlineStore.Data/Repositories/LabelRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineStore.Data.Repositories
+{
+    using System.Linq;
+
+    using OnlineStore.Data.Models;
+    using OnlineStore.Data.Repositories.Contracts;
+
+    public class LabelRepository : DeletableEntityRepository<Label>, ILabelRepository
+    {
+        public LabelRepository(IOnlineStoreDbContext context)
+            : base(context)
+        {
+        }
+
+        public IQueryable<Label> GetByProductId(int id)
+        {
+            return this.Context.Labels.Where(l => l.Products.Select(p => p.Id).Contains(id));
+        }
+    }
+}
