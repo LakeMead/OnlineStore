ac8e45ca2d2c4647550fdc5c5aeee2fbf53c31fb
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-05-17 13:24:55 +0300

    Add interface for UserRepository

---
+++ OnlineStore/OnlineStore.Data/Repositories/Contracts/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineStore.Data.Repositories.Contracts
+{
+    using System.Linq;
+
+    using OnlineStore.Data.Contracts;
+    using OnlineStore.Data.Models;
+
+    public interface IUserRepository : IDeletableEntityRepository<User>
+    {
+        IQueryable<User> GetAllOrderedProducts(string id);
+
+        IQueryable<User> GetAllWishProducts(string id);
+
+        IQueryable<User> GetLastRegistered();
+
+        IQueryable<User> SortedByOrdersCount();
+
+        User GetByUsername(string username);
+    }
+}
