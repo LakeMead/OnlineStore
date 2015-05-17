98df07351fdaa897dc836de0cb6bcf972ef1031b
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-17 18:06:43 +0300

    Added IRatingRepository.cs and RatingRepository.cs

---
+++ OnlineStore/OnlineStore.Data/Repositories/Contracts/IRatingRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineStore.Data.Repositories.Contracts
+{
+    using System.Linq;
+
+    using OnlineStore.Data.Contracts;
+    using OnlineStore.Data.Models;
+
+    public interface IRatingRepository : IDeletableEntityRepository<Rating>
+    {
+        IQueryable<Rating> GetByAuthorId(string id);
+
+        IQueryable<Rating> GetByProductId(int id);
+    }
+}
---
+++ OnlineStore/OnlineStore.Data/Repositories/RatingRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace OnlineStore.Data.Repositories
+{
+    using System.Linq;
+
+    using OnlineStore.Data.Models;
+    using OnlineStore.Data.Repositories.Contracts;
+
+    public class RatingRepository : DeletableEntityRepository<Rating>, IRatingRepository
+    {
+        public RatingRepository(IOnlineStoreDbContext context)
+            : base(context)
+        {
+        }
+
+        public IQueryable<Rating> GetByAuthorId(string id)
+        {
+            return this.Context.Ratings.Where(r => r.AuthorId == id);
+        }
+
+        public IQueryable<Rating> GetByProductId(int id)
+        {
+            return this.Context.Ratings.Where(r => r.ProductId == id);
+        }
+    }
+}
