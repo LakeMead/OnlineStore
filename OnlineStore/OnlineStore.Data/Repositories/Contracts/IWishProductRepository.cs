e978fa23714ccc41aabb08eb26d676c8a064bb9e
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-17 18:10:28 +0300

    Added IWishProductRepository.cs and WishProductRepository.cs

---
+++ OnlineStore/OnlineStore.Data/Repositories/Contracts/IWishProductRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineStore.Data.Repositories.Contracts
+{
+    using System.Linq;
+
+    using OnlineStore.Data.Contracts;
+    using OnlineStore.Data.Models;
+
+    public interface IWishProductRepository : IDeletableEntityRepository<WishProduct>
+    {
+        IQueryable<WishProduct> GetByUserId(string id);
+    }
+}
---
+++ OnlineStore/OnlineStore.Data/Repositories/WishProductRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineStore.Data.Repositories
+{
+    using System.Linq;
+
+    using OnlineStore.Data.Models;
+    using OnlineStore.Data.Repositories.Contracts;
+
+    public class WishProductRepository : DeletableEntityRepository<WishProduct>, IWishProductRepository
+    {
+        public WishProductRepository(IOnlineStoreDbContext context)
+            : base(context)
+        {
+        }
+
+        public IQueryable<WishProduct> GetByUserId(string id)
+        {
+            return this.Context.WishProducts.Where(wp => wp.UserId == id);
+        }
+    }
+}
