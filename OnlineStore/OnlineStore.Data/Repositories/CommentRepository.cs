9b5c5645c4fa26afc7e54479fa75d81e28467630
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-17 17:47:28 +0300

    added CommentRepository.cs and ICommentRepository.cs

---
+++ OnlineStore/OnlineStore.Data/Repositories/CommentRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace OnlineStore.Data.Repositories
+{
+    using System.Linq;
+
+    using OnlineStore.Data.Models;
+    using OnlineStore.Data.Repositories.Contracts;
+
+    public class CommentRepository : DeletableEntityRepository<Comment>, ICommentRepository
+    {
+        public CommentRepository(IOnlineStoreDbContext context)
+            : base(context)
+        {
+        }
+
+        public IQueryable<Comment> GetByAuthorId(string id)
+        {
+            return this.Context.Comments.Where(c => c.AuthorId.Equals(id));
+        }
+
+        public IQueryable<Comment> GetByProductId(int id)
+        {
+            return this.Context.Comments.Where(c => c.ProductId == id);
+        }
+    }
+}
---
+++ OnlineStore/OnlineStore.Data/Repositories/Contracts/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineStore.Data.Repositories.Contracts
+{
+    using System.Linq;
+
+    using OnlineStore.Data.Contracts;
+    using OnlineStore.Data.Models;
+
+    public interface ICommentRepository : IDeletableEntityRepository<Comment>
+    {
+        IQueryable<Comment> GetByAuthorId(string id);
+
+        IQueryable<Comment> GetByProductId(int id);
+    }
+}
