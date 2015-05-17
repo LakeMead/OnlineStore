5e6bb7bada8d8de2310a8b1cde9b2f4f9ceed854
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-17 18:01:17 +0300

    Added IOrderRepository.cs and OrderRepository.cs

---
+++ OnlineStore/OnlineStore.Data/Repositories/Contracts/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineStore.Data.Repositories.Contracts
+{
+    using System.Linq;
+
+    using OnlineStore.Common.Enumerations;
+    using OnlineStore.Data.Contracts;
+    using OnlineStore.Data.Models;
+
+    public interface IOrderRepository : IDeletableEntityRepository<Order>
+    {
+        IQueryable<Order> GetByOrderStatus(OrderStatus status);
+    }
+}
---
+++ OnlineStore/OnlineStore.Data/Repositories/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace OnlineStore.Data.Repositories
+{
+    using System.Linq;
+
+    using OnlineStore.Common.Enumerations;
+    using OnlineStore.Data.Models;
+    using OnlineStore.Data.Repositories.Contracts;
+
+    public class OrderRepository : DeletableEntityRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(IOnlineStoreDbContext context)
+            : base(context)
+        {
+        }
+
+        public IQueryable<Order> GetByOrderStatus(OrderStatus status)
+        {
+            return this.Context.Orders.Where(o => o.OrderStatus == status);
+        }
+    }
+}
