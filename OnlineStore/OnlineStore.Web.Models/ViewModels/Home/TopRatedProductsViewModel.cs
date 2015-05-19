d1725c96aca740a662063104b5000ec4aef33bb2
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-19 17:28:11 +0300

    TopRatedProductsViewModel.cs Created

---
+++ OnlineStore/OnlineStore.Web.Models/ViewModels/Home/TopRatedProductsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineStore.Web.Models.ViewModels.Home
+{
+    public class TopRatedProductsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
