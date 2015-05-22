b30975fbdf6009512930b2b8b3d3954b51706c69
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-05-23 00:05:09 +0300

    Added new project with empty interface for Shopping Cart Provider  #22

---
+++ OnlineStore/ShoppingCartProvider/IShoppingCartProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingCartProvider
+{
+    public interface IShoppingCartProvider
+    {
+        void AddToCart();
+        void RemoveFromCart();
+        void EmptyCart();
+        void GetCartItems();
+        void GetCount();
+        void GetTotal();
+        void CreateOrder();
+        void GetCart();
+
+
+    }
+}
