cf861cef7eeaa7e0644c6df53cf55dd6020d6fbd
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-22 17:38:08 +0300

    the discounted price is listed for discounted products #14

--- OnlineStore/OnlineStore.Web.Models/ViewModels/Home/DiscountedProductsViewModel.cs
+++ OnlineStore/OnlineStore.Web.Models/ViewModels/Home/DiscountedProductsViewModel.cs
@@ -1,20 +1,31 @@
 ﻿namespace OnlineStore.Web.Models.ViewModels.Home
 {
+    using System;
+    using System.Linq;
+
+    using AutoMapper;
+
     using OnlineStore.Common.Mapping;
     using OnlineStore.Data.Models;
 
-    public class DiscountedProductsViewModel : IMapFrom<Product>
+    public class DiscountedProductsViewModel : IMapFrom<Product>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
 
-        public decimal Price { get; set; }
+        public double NewPrice { get; set; }
 
         public int? DiscountId { get; set; }
 
         public virtual Discount Discount { get; set; }
 
         public string ImagePath { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Product, DiscountedProductsViewModel>()
+                .ForMember(m => m.NewPrice, opt => opt.MapFrom(e => (double)e.Price * e.Discount.Percent / 100));
+        }
     }
 }
