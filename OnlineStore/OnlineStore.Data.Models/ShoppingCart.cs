3d114cbee145ec91c8985b7813285b2bac81d15e
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-05-23 00:04:16 +0300

    Added model for ShoppingCart closes #23

---
+++ OnlineStore/OnlineStore.Data.Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineStore.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using OnlineStore.Data.Contracts;
+
+    public class ShoppingCart : AuditInfo
+    {
+        [Key]
+        public int RecordId { get; set; }
+
+        public string CartId { get; set; }
+
+        public int Count { get; set; }
+
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
