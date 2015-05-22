48f445eb9cbb30bd2be6b6f19f0e3af5a6636d97
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-05-22 23:46:40 +0300

    Added empty Controller for ShoppingCart

---
+++ OnlineStore/OnlineStore.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineStore.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using OnlineStore.Data;
+
+    public class ShoppingCartController : BaseController
+    {
+        public ShoppingCartController(IOnlineStoreData data) : base(data)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
