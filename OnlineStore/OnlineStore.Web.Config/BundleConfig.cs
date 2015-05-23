7f014139adc3860d27bc500aaa334b11b701a2cd
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-05-23 20:17:18 +0300

    Added jquery.unobtrusive-ajax.js for enable popup window in ajax submitting form and added to jquery bundle

--- OnlineStore/OnlineStore.Web.Config/BundleConfig.cs
+++ OnlineStore/OnlineStore.Web.Config/BundleConfig.cs
@@ -22,7 +22,8 @@
         private static void AddScripts(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                       "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
