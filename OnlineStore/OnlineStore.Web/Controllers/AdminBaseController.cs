69ec948f7ad1c6b1a1fac9d97e704fcda2ddd214
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-05-25 22:14:13 +0300

    Added AdminBaseController.cs with filter for admin roles closes #26

---
+++ OnlineStore/OnlineStore.Web/Controllers/AdminBaseController.cs
@@ -0,0 +1,16 @@
+﻿namespace OnlineStore.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using OnlineStore.Data;
+    using OnlineStore.Services.ShoppingCartProvider.Contracts;
+
+    // [Authorize(Roles = GlobalConstants.AdminRole)]
+    public abstract class AdminBaseController : BaseController
+    {
+        public AdminBaseController(IOnlineStoreData data, IShoppingCartProvider shoppingCartProvider)
+            : base(data, shoppingCartProvider)
+        {
+        }
+    }
+}
