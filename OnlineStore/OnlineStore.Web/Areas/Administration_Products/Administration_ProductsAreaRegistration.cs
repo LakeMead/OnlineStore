b88fac57674e041faadf4420f14b39d522e561fe
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-05-25 22:13:32 +0300

    Added new area for Products administration

---
+++ OnlineStore/OnlineStore.Web/Areas/Administration_Products/Administration_ProductsAreaRegistration.cs
@@ -0,0 +1,23 @@
+﻿namespace OnlineStore.Web.Areas.Administration_Products
+{
+    using System.Web.Mvc;
+
+    public class AdministrationProductsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Administration_Products";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Administration_Products_default",
+                "Administration_Products/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
