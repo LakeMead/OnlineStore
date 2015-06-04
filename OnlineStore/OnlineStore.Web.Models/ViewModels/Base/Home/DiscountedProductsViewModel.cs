f80c0351939d0d1e6a5f5c8c5c45ea963feab473
Geogri Manov <georgimanov@gmail.com>
2015-06-04 10:54:36 +0300

    work on styles

--- OnlineStore/OnlineStore.Web.Models/ViewModels/Base/Home/DiscountedProductsViewModel.cs
+++ OnlineStore/OnlineStore.Web.Models/ViewModels/Base/Home/DiscountedProductsViewModel.cs
@@ -11,6 +11,8 @@
 
         public string Name { get; set; }
 
+        public decimal Price { get; set; }
+
         public decimal NewPrice { get; set; }
 
         public int? DiscountId { get; set; }
